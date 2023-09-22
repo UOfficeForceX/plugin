e941846b1de571abe41425836b1685e892ad94e7
holly <holly@edetw.com>
2023-09-22 11:19:21 +0800

    移除部分啟動內容

--- Ede.Uofx.Customize.Web/ClientApp/src/bootstrap.ts
+++ Ede.Uofx.Customize.Web/ClientApp/src/bootstrap.ts
@@ -2,10 +2,6 @@ import { AppModule } from './app/app.module';
 import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { registerLicense } from '@syncfusion/ej2-base';
-
-// Registering Syncfusion license key
-registerLicense('ORg4AjUWIQA/Gnt2V1hhQlJAfV5AQmBIYVp/TGpJfl96cVxMZVVBJAtUQF1hSn5Ud0RhWHxZcHVURWha');
 
 if (environment.production) {
   enableProdMode();
